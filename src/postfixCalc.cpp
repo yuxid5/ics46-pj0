e7a711e56057f68e445c606a0aba930aebe4a7a0
Yuxi Dai <yuxid5@uci.edu>
2024-01-14 07:22:04 +0000

    finished postfix and pass the test

--- src/postfixCalc.cpp
+++ src/postfixCalc.cpp
@@ -6,7 +6,54 @@ namespace shindler::ics46::project0 {
 
 unsigned postfixCalculator(const std::vector<std::string>& entries) {
     // TODO: Implement Function
-    return {};  // stub, not the correct answer
-}
+    // stub, not the correct answer
+    LinkedListStack<unsigned> number_stack;
+    for(std::string i : entries)
+    {
+        if (i == "+"){
+            unsigned first_number = number_stack.top();
+            number_stack.pop();
+            unsigned second_number = number_stack.top();
+            number_stack.pop();
+            unsigned result_number = first_number + second_number;
+            number_stack.push(result_number);
+        }
+        else if (i == "-"){
 
+            unsigned first_number = number_stack.top();
+            number_stack.pop();
+            unsigned second_number = number_stack.top();
+            number_stack.pop();
+            unsigned result_number = second_number - first_number;
+            number_stack.push(result_number);
+        }
+        else if (i == "*"){
+
+            unsigned first_number = number_stack.top();
+            number_stack.pop();
+            unsigned second_number = number_stack.top();
+            number_stack.pop();
+            unsigned result_number = first_number * second_number;
+            number_stack.push(result_number);
+        }
+        else if (i == "/"){
+
+            unsigned first_number = number_stack.top();
+            number_stack.pop();
+            unsigned second_number = number_stack.top();
+            number_stack.pop();
+            unsigned result_number = second_number / first_number;
+            number_stack.push(result_number);
+        }
+        else{
+            number_stack.push(std::stoul(i));
+        }
+    }
+    if (number_stack.size() == 1){
+        return number_stack.top();
+    }
+    else{
+        throw CannotEvaluateException{"Error!"};
+    }
+}
 }  // namespace shindler::ics46::project0
