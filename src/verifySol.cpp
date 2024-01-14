3d6c6d20178e95bd18358e61c2b29bd9f4e80679
Yuxi Dai <yuxid5@uci.edu>
2024-01-14 04:40:47 +0000

    made program more clear

--- src/verifySol.cpp
+++ src/verifySol.cpp
@@ -10,31 +10,26 @@ bool verifySolution(const std::string& addend1, const std::string& addend2,
                     const std::unordered_map<char, unsigned>& mapping) {
     // TODO: Implement
     int addend1_number = 0;
-    for (char i : addend1){
-        int current_number = mapping.at(i);
+    for (char current_char : addend1){
+        int current_number = mapping.at(current_char);
         addend1_number = addend1_number * 10 + current_number;
     }
 
     int addend2_number = 0;
-    for (char i : addend2){
-        int current_number = mapping.at(i);
+    for (char current_char : addend2){
+        int current_number = mapping.at(current_char);
         addend2_number = addend2_number * 10 + current_number;
     }
 
     int result_number = 0;
-    for (char i : result){
-        int current_number = mapping.at(i);
+    for (char current_char : result){
+        int current_number = mapping.at(current_char);
         result_number = result_number * 10 + current_number;
     }
 
     int real_result = addend1_number + addend2_number;
 
-    if (real_result != result_number){
-        return false;
-    }
-    else{
-        return true;
-    }
+    return real_result == result_number;
 }
 
 }  // namespace shindler::ics46::project0
