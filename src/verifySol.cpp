bd75b68a8a13b576b787710cf63b1370a469a73a
Yuxi Dai <yuxid5@uci.edu>
2024-01-14 04:18:24 +0000

    fix my verify

--- src/verifySol.cpp
+++ src/verifySol.cpp
@@ -9,31 +9,28 @@ bool verifySolution(const std::string& addend1, const std::string& addend2,
                     const std::string& result,
                     const std::unordered_map<char, unsigned>& mapping) {
     // TODO: Implement
-    std::string addend1_str;
-    for (int i{0}; i < addend1.length(); i++){
-        std::string number = std::to_string(mapping.at(addend1[i]));
-        addend1_str += number;
+    int addend1_number = 0;
+    for (char i : addend1){
+        int current_number = mapping.at(i);
+        addend1_number = addend1_number * 10 + current_number;
     }
-    int addend1_num = stoi(addend1_str)
 
-    std::string addend2_str;
-    for (int i{0}; i < addend2.length(); i++){
-        std::string number = std::to_string(mapping.at(addend2[i]));
-        addend2_str += number;
+    int addend2_number = 0;
+    for (char i : addend2){
+        int current_number = mapping.at(i);
+        addend2_number = addend2_number * 10 + current_number;
     }
-    int addend2_num = stoi(addend2_str)
 
-    std::string result_str;
-    for (int i{0}; i < result.length(); i++){
-        std::string number = std::to_string(mapping.at(result[i]));
-        result_str += number;
+    int result_number = 0;
+    for (char i : result){
+        int current_number = mapping.at(i);
+        result_number = result_number * 10 + current_number;
     }
-    int result_num = stoi(addend2_str)
 
-    int real_result = addend1_num + addend2_num;
+    int real_result = addend1_number + addend2_number;
 
-    if (real_result != result_num){
-        retrun false;
+    if (real_result != result_number){
+        return false;
     }
     else{
         return true;
