f95b3cadfe9258c6735b06fb09fa8273fa4fb8ec
Yuxi Dai <yuxid5@uci.edu>
2024-01-14 06:15:20 +0000

    add linked list

--- src/LinkedListStack.hpp
+++ src/LinkedListStack.hpp
@@ -16,6 +16,16 @@ class StackEmptyException : public std::runtime_error {
 template <typename T>
 class LinkedListStack {
    private:
+        struct Node{
+            Node(T v)
+            : value{v}, next{nullptr}
+            {}
+            T value;
+            Node * next;
+        };
+        Node * first;
+        Node * last;
+        size_t count;
     // fill in private member data here
 
    public:
@@ -49,37 +59,77 @@ class LinkedListStack {
 };
 
 template <typename T>
-LinkedListStack<T>::LinkedListStack() {
+LinkedListStack<T>::LinkedListStack() 
+    :first{nullptr}, last(nullptr),count(0)
+{
     // TODO: Fill in your constructor implementation here.
 }
 
 template <typename T>
-LinkedListStack<T>::LinkedListStack(const LinkedListStack& stackb) {
+LinkedListStack<T>::LinkedListStack(const LinkedListStack& stackb)
+    :first{nullptr}, last{nullptr}, count(0)
+{
     // TODO: Fill in your copy constructor implementation here.
+    Node* current_copy = stackb.first;
+    while(current_copy->next != nullptr){
+        Node* addToBack = new Node(current_copy->value);
+        if (first == nullptr)
+        {
+            first = last = addToBack;
+        }
+        else{
+            last->next = addToBack;
+            last = addToBack;
+        }
+        count++;
+        current_copy = current_copy->next;
+    }
 }
 
 template <typename T>
 LinkedListStack<T>& LinkedListStack<T>::operator=(
     const LinkedListStack& stackb) {
     // TODO: Fill in your assignment operator implementation here.
-    return *this;  // Stub so this function compiles without implementation.
+    // Stub so this function compiles without implementation.
+    if (this != &stackb){
+        while(!isEmpty){
+            pop()
+        }
+    }
+    Node* current_copy = stackb.first;
+    while(current_copy->next != nullptr){
+        Node* addToBack = new Node(current_copy->value);
+        if (first == nullptr)
+        {
+            first = last = addToBack;
+        }
+        else{
+            last->next = addToBack;
+            last = addToBack;
+        }
+        count++;
+        current_copy = current_copy->next;
+    }
 }
 
 template <typename T>
 LinkedListStack<T>::~LinkedListStack() {
     // TODO: Fill in your destructor implementation here.
+    while (!isEmpty()){
+        pop();
+    }
 }
 
 template <typename T>
 size_t LinkedListStack<T>::size() const noexcept {
     // TODO: Fill in your size() implementation here.
-    return {};  // Stub so this function compiles without an implementation.
+    return count;  // Stub so this function compiles without an implementation.
 }
 
 template <typename T>
 bool LinkedListStack<T>::empty() const noexcept {
     // TODO: Fill in your isEmpty() implementation here.
-    return {};  // Stub so this function compiles without an implementation.
+    return first == nullptr;  // Stub so this function compiles without an implementation.
 }
 
 template <typename T>
@@ -87,8 +137,13 @@ T& LinkedListStack<T>::top() {
     // TODO: Fill in your top() implementation here.
     // The following is a stub just to get the template project to compile.
     // You should delete it for your implementation.
-    auto val = new T;
-    return *val;
+    if (isEmpty())
+    {
+        throw StackEmptyException{"Stack is Empty"}
+    }
+    else{
+        return first -> value;
+    }
 }
 
 template <typename T>
@@ -96,17 +151,42 @@ const T& LinkedListStack<T>::top() const {
     // TODO: Fill in your const top() implementation here.
     // The following is a stub just to get the template project to compile.
     // You should delete it for your implementation.
-    return {};
+    if (isEmpty())
+    {
+        throw StackEmptyException{"Stack is Empty"}
+    }
+    else{
+        return first -> value;
+    }
 }
 
 template <typename T>
 void LinkedListStack<T>::push(const T& elem) noexcept {
     // TODO: Fill in your push() implementation here.
+    Node* addToFront = new Node(elem);
+    if (first == nullptr)
+    {
+        first = last = addToFront;
+    }
+    else
+    {
+        addToFront -> next = first;
+        first = addToFront;
+    }
+    count++;
 }
 
 template <typename T>
 void LinkedListStack<T>::pop() {
     // TODO: Fill in your pop() implementation here.
+    if (isEmpty())
+    {
+        throw StackEmptyException{"Stack is Empty"}
+    }
+    Node * temp = first;
+    first = first->next;
+    delete temp;
+    count--;
 }
 
 }  // namespace shindler::ics46::project0
